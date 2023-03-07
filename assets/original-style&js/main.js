9bd106f7f6f734e44c7963601ec15458ca693fb3
ObradPR <100384517+ObradPR@users.noreply.github.com>
2023-03-07 08:00:23 +0100

    Add files via upload

--- assets/original-style&js/main.js
+++ assets/original-style&js/main.js
@@ -7,6 +7,7 @@ const collapseMenuButton = document.querySelector(`#toggle`);
 const navMeni = document.querySelector(`#nav__menu`);
 const cartBtn = document.querySelector(`#cart`);
 const logo = document.querySelector(`#logo`);
+let data = JSON.parse(localStorage.getItem("zaKupovinu"));
 
 // GLOBAL AJAX CALLS
 ajaxCall("navMeni", ispisNavMeni);
@@ -26,19 +27,21 @@ if (url === "/FlexFitnessSupply/" || url === "/FlexFitnessSupply/index.html") {
   const bestProductsContainer = document.querySelector(
     `#best__products_container`
   );
-
-  $(document).ready(function () {
-    // INDEX PROMOTION SLDIER
-    $(`#slider__promotion`).skdslider({
-      slideSelector: `.slide__promotion`,
-      delay: 4000,
-      animationSpeed: 1000,
-      showNextPrev: false,
-      showPlayButton: false,
-      autoSlide: true,
-      animationType: `sliding`,
-    });
-  });
+  let timer = setInterval(() => {
+    if ($(`.slide__promotion`) != undefined && $(`.add__to_cart`) != undefined) {
+      $(`#slider__promotion`).skdslider({
+        slideSelector: `.slide__promotion`,
+        delay: 4000,
+        animationSpeed: 1000,
+        showNextPrev: false,
+        showPlayButton: false,
+        autoSlide: true,
+        animationType: `sliding`,
+      });
+      dugmeZaDodavanjeFProizvodima();
+      clearInterval(timer);
+    }
+  }, 100);
 
   // INDEX AJAX CALLS
   ajaxCall("products", ispisNajprodavanijihProizvoda);
@@ -47,6 +50,7 @@ if (url === "/FlexFitnessSupply/" || url === "/FlexFitnessSupply/index.html") {
   // INDEX FUNCTIONS
   function ispisNajprodavanijihProizvoda(data) {
     let sadrzaj = "";
+    proizvodi = data;
 
     for (let obj of data) {
       if (obj.najprodavaniji) {
@@ -92,32 +96,16 @@ if (url === "/FlexFitnessSupply/" || url === "/FlexFitnessSupply/index.html") {
   searchTb.addEventListener(`keyup`, filterChange);
 
   // ADD PRODUCT TO LOCAL STORAGE EVENT
-  $(document).ready(function () {
-    let data;
-    data = JSON.parse(localStorage.getItem("zaKupovinu"));
-    if (data == null) {
-      data = [];
+  
+  let timer2 = setInterval(() => {
+    if ($(`.add__to_cart`) != undefined) {
+      dugmeZaDodavanjeFProizvodima();
+      clearInterval(timer2);
     }
-    document.querySelectorAll(`.add__to_cart`).forEach((btn) => {
-      btn.addEventListener(`click`, function (e) {
-        e.preventDefault();
-
-        setTimeout(() => {
-          document.querySelector(`#success__cart`).style.display = `none`;
-        }, 900);
-        document.querySelector(`#success__cart`).style.display = `block`;
-
-        let id = btn.nextSibling.nextSibling.defaultValue;
-        let p = proizvodi.find((x) => x.id == id);
-        if (p.dostupnost) {
-          data.push(p);
-          dodajULocalStorage(data);
-        }
-      });
-    });
-  });
+  }, 100);
 
   // SHOP FUNCTIONS
+  
   function ispisSidebarTeksta(data, classs) {
     let sadrzaj = "";
     let value = 1;
@@ -546,7 +534,7 @@ function ispisProizvodBloka(obj, txtVar) {
     <p>${obj.cena.aktuelnaCena} RSD</p>
     <del>${obj.cena.staraCena ? obj.cena.staraCena + " RSD" : ""}</del>
     </div>
-    <a href="#" class="add__to_cart"><i class="fa-solid fa-bag-shopping"></i></a>
+    <div class="add__to_cart"><i class="fa-solid fa-bag-shopping"></i></div>
     <input type="hidden" value="${obj.id}"/>
   </div>
 </div>`;
@@ -579,6 +567,26 @@ function ispisNavMeni(data) {
     meni += `<li><a href="${obj.href}">${obj.naziv}</a></li>`;
   }
   $("#nav__menu").html(meni);
+}
+function dugmeZaDodavanjeFProizvodima() {
+  if (data == null) {
+    data = [];
+  }
+  document.querySelectorAll(`.add__to_cart`).forEach((btn) => {
+    btn.addEventListener(`click`, function () {
+      setTimeout(() => {
+        document.querySelector(`#success__cart`).style.display = `none`;
+      }, 900);
+      document.querySelector(`#success__cart`).style.display = `block`;
+
+      let id = btn.nextSibling.nextSibling.defaultValue;
+      let p = proizvodi.find((x) => x.id == id);
+      if (p.dostupnost) {
+        data.push(p);
+        dodajULocalStorage(data);
+      }
+    });
+  });
 }
 function dodajULocalStorage(data) {
   localStorage.setItem("zaKupovinu", JSON.stringify(data));
